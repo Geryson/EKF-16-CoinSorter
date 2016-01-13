1dc864dc69a3bcd75a7ab16fac17d4938656877b
Geryson <gcsavas@freemail.hu>
2016-01-13 14:10:52 +0100

    Servo - 36 fokonkénti fordulat beállítva

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -1,18 +1,24 @@
-//A servo működtetése csak PWM digitális porton 
-//(3 / 5 / 6 / 9 / 10 / 11 / 13) működik rendeltetésszerűen
-
 #include <Servo.h>
-Servo myServo;        // Create Servo object to control the servo 
-
+Servo myServo;
+ 
 void setup() { 
-  myServo.attach(9);  // Servo is connected to digital pin 9 
-} 
-
-void loop() { 
-  myServo.writeMicroseconds(1700);  // Counter clockwise
-  delay(2000);                      // Wait 2 seconds
-  myServo.writeMicroseconds(1300);  // Clockwise
-  delay(2000);
-  myServo.writeMicroseconds(1500);  // Stop
-  delay(2000); 
+   myServo.attach(9);
+   
+ 
 }
+void loop() {
+  
+  myServo.write(0);
+  delay(2000);
+  myServo.write(36);
+  delay(2000);
+  myServo.write(72);
+  delay(2000);
+  myServo.write(108);
+  delay(2000);
+  myServo.write(144);
+  delay(2000);
+  myServo.write(180);
+  delay(2000);
+  
+}   
